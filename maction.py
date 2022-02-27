9f2435bd751a94e1f4e563a8cb176eeb46f1c0af
Aayushman Maharjan <aayushman.maharjan0@gmail.com>
2022-02-27 14:53:38 +0545

    Update

--- maction.py
+++ maction.py
@@ -4,15 +4,27 @@ import mouse
 prev_x = 0
 prev_y = 0
 action = 0
-
+clicked = 0
 
 def mouseAction(pList, max_x, max_y):
-    global action
-    if(pList[0][1] >= 0.85):
+    global action, clicked
+    if(pList[0][0] >= 0.85 and clicked == 0):
+        #mouse.click('left')
+        clicked = 1
+    elif(pList[0][1] >= 0.85):
         action = 1
-    
-    if (action == 1):
+        clicked = 0
+    elif(pList[0][2] >= 0.85):
+        #mouse.click('right')
+        clicked = 1
+    elif(pList[0][5] >= 0.85):
+        action = 2
+        
+        
+    if(action == 1):
         mouseMove(max_x, max_y)
+    elif(action == 2):
+        scroll(max_x, max_y)
 
 def mouseMove(max_x, max_y):
     global prev_x, prev_y
@@ -29,6 +41,18 @@ def mouseMove(max_x, max_y):
         move_y = y_neg * (y_diff ** 2) / 2
         
         mouse.move(move_x, move_y, absolute = False)
+    
+    prev_x = max_x
+    prev_y = max_y
+    
+def scroll(max_x, max_y):
+    global prev_x, prev_y
+    x_diff = max_x - prev_x
+    
+    if(x_diff > 2):
+        mouse.wheel(1)
+    elif(x_diff <-2):
+        mouse.wheel(-1)
     
     prev_x = max_x
     prev_y = max_y
